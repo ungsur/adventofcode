4b43ac0d9aa42672161cb0c4a2336e01826e30eb
ungsur <ungsunan@gmail.com>
2022-01-01 07:38:31 -0500

    changes

--- 2021/day1/main.py
+++ 2021/day1/main.py
@@ -4,13 +4,11 @@ def sweep(inputfile):
         for line in fh:
             depths.append(int(line))
     counter = 0
-    for i in range(1,len(depths)):
+    for i in range(1, len(depths)):
         if depths[i-1] < depths[i]:
             counter += 1
     return counter
 
-def sweep2(list -> depthlist):
-    pass 
 
 def sweep_part2(inputfile):
     depths = []
@@ -19,20 +17,21 @@ def sweep_part2(inputfile):
             depths.append(int(line))
     counter = 0
     newdepths = []
-    for i in range(2,len(depths)):
-        depthsum  = sum(depths[i-2:i+1])
+    for i in range(2, len(depths)):
+        depthsum = sum(depths[i-2:i+1])
         newdepths.append(depthsum)
-    for i in range(1,len(newdepths)):
+    for i in range(1, len(newdepths)):
         if newdepths[i-1] < newdepths[i]:
             counter += 1
     return counter
-    
+
 
 def main():
     inputfile = './input/exampleinput'
     print(sweep_part2(inputfile))
     inputfile2 = './input/input.txt'
     print(sweep_part2(inputfile2))
+
 
 if __name__ == '__main__':
     main()
