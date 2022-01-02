d2b0334bfd9cb383614b769a02cb747ed32ae07d
ungsur <ungsunan@gmail.com>
2022-01-02 12:13:50 -0500

    code cleanup

--- 2021/day6/main.py
+++ 2021/day6/main.py
@@ -1,21 +1,10 @@
 def processiterations(fisharr: list, n: int):
-    queuelen = len(fisharr)
-    fishcounter = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
-    for val in fisharr:
-        if fishcounter.get(val):
-            fishcounter[val] += 1
-        else:
-            fishcounter[val] = 1
-    print(fishcounter)
-    for i in range(n):
-        for k, v in fishcounter.items():
-            fishcounter[k] -= 1
-    print(fishcounter)
-    return queuelen
+    print(fisharr, n)
+    return n
 
 
 def main():
-    example = [3, 4, 3, 1, 2]
+    example = [1, 2, 3, 4, 5, 6]
     print("hi")
     processiterations(example, 1)
 
