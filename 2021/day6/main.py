240529238d94f00aa745d32a2e4a057c905b29cd
ungsur <ungsunan@gmail.com>
2022-01-02 12:25:14 -0500

    commit changes

--- 2021/day6/main.py
+++ 2021/day6/main.py
@@ -1,12 +1,7 @@
-def processiterations(fisharr: list, n: int):
-    print(fisharr, n)
-    return n
-
-
 def main():
     example = [1, 2, 3, 4, 5, 6]
     print("hi")
-    processiterations(example, 1)
+    print(example)
 
 
 if __name__ == "__main__":
