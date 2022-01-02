a5cd882f17dc616dd9ed8fc4bfbe334581b83f63
ungsur <ungsunan@gmail.com>
2022-01-02 12:32:15 -0500

    blah

--- 2021/day6/main.py
+++ 2021/day6/main.py
@@ -1,4 +1,11 @@
+"""This script prompts a user to enter a message to encode or decode
+ 3  using a classic Caeser shift substitution (3 letter shift)"""
+
+
 def main():
+    """
+    here is my docstring
+    """
     pass
 
 
