466de3d995d441dd12cb719f7a9dcea6831a2988
ungsur <ungsunan@gmail.com>
2022-01-09 00:08:08 -0500

    day9 part 2 done!

--- 2021/day9/main.py
+++ 2021/day9/main.py
@@ -10,7 +10,7 @@ def getinput(filename: str) -> list:
     return grid
 
 
-def checkpoint(grid: list[list], coordinate: tuple):
+def checkpoint(grid, coordinate):
     """docstring to check if point is a lowpoint"""
     x, y = coordinate
     neighbors = []
@@ -24,22 +24,71 @@ def checkpoint(grid: list[list], coordinate: tuple):
             neighbors.append(grid[neighbor_x][neighbor_y])
     if grid[x][y] < min(neighbors):
         return grid[x][y]
+    return None
 
 
 def traversegrid(grid):
     """see grid"""
+    lowpoints = []
+    for i, _ in enumerate(grid):
+        for j, _ in enumerate(grid[i]):
+            if isinstance(checkpoint(grid, (i, j)), int):
+                lowpoints.append(checkpoint(grid, (i, j)) + 1)
+    return sum(lowpoints)
+
+
+def get_lowpoints(grid):
+    """see grid"""
     ans = []
-    for i in range(len(grid)):
+    lowpoints = []
+    for i, _ in enumerate(grid):
         for j in range(len(grid[i])):
-            if type(checkpoint(grid, (i, j))) == int:
+            if isinstance(checkpoint(grid, (i, j)), int):
                 ans.append(checkpoint(grid, (i, j)))
-    return sum(ans) + len(ans)
+                lowpoints.append([i, j])
+    return lowpoints
+
+
+def part2(grid):
+    """docstring for part 2"""
+    basin_sizes = []
+    seen = set()
+    for x, _ in enumerate(grid):
+        for y, _ in enumerate(grid[0]):
+            if (x, y) not in seen and grid[x][y] != 9:
+                basinsize = 0
+                temp_que = []
+                temp_que.append((x, y))
+                while temp_que:
+                    (x, y) = temp_que.pop(0)
+                    if (x, y) in seen:
+                        continue
+                    seen.add((x, y))
+                    basinsize += 1
+                    for neighbor_x, neighbor_y in [
+                        [x - 1, y],
+                        [x, y - 1],
+                        [x + 1, y],
+                        [x, y + 1],
+                    ]:
+                        if (
+                            0 <= neighbor_x < len(grid)
+                            and 0 <= neighbor_y < len(grid[0])
+                            and grid[neighbor_x][neighbor_y] != 9
+                        ):
+                            temp_que.append((neighbor_x, neighbor_y))
+                basin_sizes.append(basinsize)
+    basin_sizes.sort()
+    print(basin_sizes)
+    return basin_sizes[-3] * basin_sizes[-2] * basin_sizes[-1]
 
 
 def main():
     """main function docstring"""
     cavern = getinput("input/example.txt")
     print(traversegrid(cavern))
+    print(get_lowpoints(cavern))
+    print(part2(cavern))
 
 
 if __name__ == "__main__":
