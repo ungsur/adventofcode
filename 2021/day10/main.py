9d078135546844f62f9fc836b6520e25760cdb6f
ungsur <ungsunan@gmail.com>
2022-01-09 18:41:18 -0500

    Day 10 part2 solved

--- 2021/day10/main.py
+++ 2021/day10/main.py
@@ -1,9 +1,6 @@
 """Advent of code day 10"""
 
 
-from sys import base_exec_prefix
-
-
 def getinput(inputfile):
     inputlist = []
     with open(inputfile) as f_handle:
@@ -23,6 +20,21 @@ def bracketcheck(word):
             brack = temp_stack.pop()
             if lookup[letter] != brack:
                 return letter
+    return None
+
+
+def bracketcheck_part2(word):
+    lookup = {"}": "{", ")": "(", "]": "[", ">": "<"}
+    temp_stack = []
+
+    for letter in list(word):
+        if letter in ["(", "{", "[", "<"]:
+            temp_stack.append(letter)
+        elif letter in [")", "}", "]", ">"]:
+            brack = temp_stack.pop()
+            if lookup[letter] != brack:
+                return []
+    return temp_stack
 
 
 def inputsum(inputlist):
@@ -37,10 +49,31 @@ def inputsum(inputlist):
     return totalsum
 
 
+def inputsum_part2(inputlist):
+    vals = {"}": 3, ")": 1, "]": 2, ">": 4}
+    lookup = {"{": "}", "(": ")", "[": "]", "<": ">"}
+    total = 0
+    for brack in inputlist[::-1]:
+        total = total * 5
+        total += vals[lookup[brack]]
+    return total
+
+
 def main():
     inputlist = getinput("input/input.txt")
     print(inputlist)
     print(inputsum(inputlist))
+
+    print(bracketcheck_part2("[({(<(())[]>[[{[]{<()<>>"))
+    print(inputsum_part2(bracketcheck_part2("<{([{{}}[<[[[<>{}]]]>[]]")))
+    stacks = []
+    for bracketline in inputlist:
+        total = inputsum_part2(bracketcheck_part2(bracketline))
+        if total > 0:
+            stacks.append(total)
+    mid = len(stacks) // 2
+    print(sorted(stacks))
+    print(sorted(stacks)[mid])
 
 
 if __name__ == "__main__":
