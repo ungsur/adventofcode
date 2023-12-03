82e86f04860ab782bdc5abc093167eab9df9f50c
ungsur <ungsunan@gmail.com>
2023-12-03 13:48:33 -0500

    day2part1done

--- 2023/day2/main.py
+++ 2023/day2/main.py
@@ -5,24 +5,31 @@ def count_cubes(inputfile):
     '''print out numbers in inputfile'''
 
     def process_game(gamestr):
-        print(gamestr.replace(";", ","))
+        print(gamestr.strip())
         cube_limits = {'red': 12, 'green': 13, 'blue': 14}
-        gameset = gamestr.replace(";",",").strip().split(':')[1].split(',')
-        for item in gameset:
-            ballnum, ballcol = item.split()
-            ballnum = int(ballnum)
-            cube_limits[ballcol] -= ballnum
+
+        gamenum = int(gamestr.strip('\n').split(':')[0].split('Game ')[1])
+        gamesets = gamestr.strip('\n').split(': ')[1].split('; ')
         print(cube_limits)
-        if cube_limits['red'] >= 0 and cube_limits['green'] >= 0 and cube_limits['blue'] >= 0:
-            return (int(gamestr.split(":")[0].split()[1]))
-        else:
-            return 0
+        for game in gamesets:
+            cube_limits = {'red': 12, 'green': 13, 'blue': 14}
+            thingstack = []
+            for thing in game.split(','):
+                thingstack.append(thing.split())
+            while thingstack:    
+                ballnum, ballcol = thingstack.pop()
+                cube_limits[ballcol] -= int(ballnum)
+                print(cube_limits)
+                if cube_limits[ballcol] < 0:
+                    return 0
+            print(gamenum)
+        return gamenum
 
     with open("input/" + inputfile, encoding='UTF-8') as fh:
         total = 0
         for line in fh:
             total += process_game(line.strip('\''))
-        return total 
+        return total
 
 
 def main():
