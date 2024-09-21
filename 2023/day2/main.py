b6597115dc31e57123322166907c359829c6b4bb
Richard Ungsunan <494272+ungsur@users.noreply.github.com>
2024-09-21 17:26:30 -0400

    day2 redone

--- 2023/day2/main.py
+++ 2023/day2/main.py
@@ -1,4 +1,49 @@
 """ advent of code 2023 day 2 """
+import typing as t
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Round:
+    red: int = 0
+    blue: int = 0
+    green: int = 0
+
+    @classmethod
+    def from_input(cls, text:str) -> t.Self:
+        parts = [pair.strip().partition(" ") for pair in text.split(",")]
+        return cls(**{name.strip(): int(value) for value, _ , name in parts})
+@dataclass
+class Game:
+    id: int
+    rounds: t.Sequence[Round]
+
+    @classmethod
+    def from_line(cls, line:str) -> t.Self:
+        identifier, _, remainder = line.partition(":")
+        number = int(identifier.split()[1])
+        rounds = [Round.from_input(part.strip()) for part in remainder.split(";")]
+        return cls(number, tuple(rounds))
+    
+    def is_possible(self, red: int, green: int, blue: int)->bool:
+        return all(round.red<=red and round.green <= green and round.blue <= blue
+                   for round in self.rounds
+                   )
+    
+
+class PowerGame(Game):
+    @property
+    def power(self) -> int:
+        red = green = blue = 0
+        for round in self.rounds:
+            red = max(red, round.red)
+            green = max(green, round.green)
+            blue = max(blue, round.blue)
+        return red * green * blue
+    
+
+def count_cubes_2024(inputfile):
+    '''print sum of ID's for valid games'''
 
 
 def count_cubes(inputfile):
@@ -57,9 +102,16 @@ def count_cubes(inputfile):
 
 
 def main():
+    import aocd
+    games =  [Game.from_line(line) for line in aocd.get_data(day=2, year=2023).splitlines()]
+    print(sum(game.id for game in games if game.is_possible(12, 13, 14)))
+    
+    games =  [PowerGame.from_line(line) for line in aocd.get_data(day=2, year=2023).splitlines()]
+    print(sum(game.power for game in games))
+    
     """main function"""
     '''print(count_cubes("input1.txt"))'''
-    print(count_cubes("input1.txt"))
+    #print(count_cubes("input1.txt"))
 
 if __name__ == '__main__':
     main()
